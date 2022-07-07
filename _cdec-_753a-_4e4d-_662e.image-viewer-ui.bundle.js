d1a85248026fa38b9ee4c75f69e1b5ef867dce0b
toloudis <danielt@alleninstitute.org>
2022-07-07 14:36:02 -0700

    Updates

--- _cdec-_753a-_4e4d-_662e.image-viewer-ui.bundle.js
+++ _cdec-_753a-_4e4d-_662e.image-viewer-ui.bundle.js
@@ -168,7 +168,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/get mini-css chunk filename */
 /******/ 	(() => {
-/******/ 		// This function allow to reference all chunks
+/******/ 		// This function allow to reference async chunks and sibling chunks for the entrypoint
 /******/ 		__webpack_require__.miniCssF = (chunkId) => {
 /******/ 			// return url for filenames based on template
 /******/ 			return undefined;
